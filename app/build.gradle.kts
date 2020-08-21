1ab3a7b3af303a9e770ad278c32f0bfd60befc6f
Yahya <yahyatinani@gmail.com>
2020-08-21 12:44:58 +0100

    Add dependencies to a multi-module project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,5 +4,9 @@ plugins {
 }
 
 dependencies {
+    val ext = project.rootProject.ext
+
     implementation(kotlin("stdlib"))
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:${ext["kotestVersion"]}")
+    testRuntimeOnly("org.slf4j;slf4j-nop:${ext["slf4jVersion"]}")
 }
